e4d0fb9b0fb63f5c71ce197ff6a89c68b274b763
doukheeWon <fain9301@candh.co.kr>
2022-10-20 18:11:24 +0900

    feat: sample code

---
+++ src/main/java/kr/co/won/cicdgradlesample/CicdGradleSampleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.won.cicdgradlesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdGradleSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdGradleSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/won/cicdgradlesample/CicdGradleSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.won.cicdgradlesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdGradleSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
